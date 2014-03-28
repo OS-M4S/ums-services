fac58c06e0e77c5288cdf0fc0ab7f9baa71714a4
Saleem Ullah <saleem.ullah.88@gmail.com>
2014-03-28 10:54:47 +0500

    + login works now. successful login returns first_name and last_name

--- m4slogin/users/resources.py
+++ m4slogin/users/resources.py
@@ -11,8 +11,25 @@ from tastypie.utils import trailing_slash
 
 from tastypie import fields
 from django.contrib.auth.models import User
-
+from django.contrib.auth import authenticate, login, logout
+from tastypie.http import HttpUnauthorized, HttpForbidden
 from django.contrib.auth.hashers import make_password
+
+import re
+
+MINIMUM_PASSWORD_LENGTH = 4
+REGEX_VALID_PASSWORD = (
+    ## Don't allow any spaces, e.g. '\t', '\n' or whitespace etc.
+    r'^(?!.*[\s])'
+    ## Check for a digit
+    '((?=.*[\d])'
+    ## Check for an uppercase letter
+    '(?=.*[A-Z])'
+    ## check for special characters. Something which is not word, digit or
+    ## space will be treated as special character
+    '(?=.*[^\w\d\s])).'
+    ## Minimum number of characters
+    '{' + str(MINIMUM_PASSWORD_LENGTH) + ',}$')
 
 
 class CreateUserResource(ModelResource):
@@ -21,7 +38,8 @@ class CreateUserResource(ModelResource):
     class Meta:
         allowed_methods = ['post']
         always_return_data = True
-        authentication = Authentication()
+        authentication = MultiAuthentication(BasicAuthentication(), ApiKeyAuthentication())
+        # authentication = Authentication()
         authorization = Authorization()
         queryset = UserProfile.objects.all()
         resource_name = 'createuser'
@@ -63,19 +81,7 @@ class CreateUserResource(ModelResource):
                     message="That email is already used.")
             # validate password
             rp = bundle.data["user"]["raw_password"]
-            if not self.validate_password(rp):
-                if len(rp) < MINIMUM_PASSWORD_LENGTH:
-                    raise CustomBadRequest(
-                        code="invalid_password",
-                        message=(
-                            "Your password should contain at least {length} "
-                            "characters.".format(length=
-                                                 MINIMUM_PASSWORD_LENGTH)))
-                raise CustomBadRequest(
-                    code="invalid_password",
-                    message=("Your password should contain at least one number"
-                             ", one uppercase letter, one special character,"
-                             " and no spaces."))
+            self.validate_password(rp)
         except KeyError as missing_key:
             raise CustomBadRequest(
                 code="missing_key",
@@ -92,35 +98,29 @@ class CreateUserResource(ModelResource):
         if re.match(REGEX_VALID_PASSWORD, password):
             return True
         # Todo fix this
+        if len(password) < MINIMUM_PASSWORD_LENGTH:
+            raise CustomBadRequest(
+                code="invalid_password",
+                message=(
+                    "Your password should contain at least {length} "
+                    "characters.".format(length=MINIMUM_PASSWORD_LENGTH)))
+        raise CustomBadRequest(
+            code="invalid_password",
+            message=("Your password should contain at least one number"
+                     ", one uppercase letter, one special character,"
+                     " and no spaces."))
         return False
 
 ################################################################################
 ################################################################################
 
 
-import re
-
-MINIMUM_PASSWORD_LENGTH = 3
-REGEX_VALID_PASSWORD = (
-    ## Don't allow any spaces, e.g. '\t', '\n' or whitespace etc.
-    r'^(?!.*[\s])'
-    ## Check for a digit
-    '((?=.*[\d])'
-    ## Check for an uppercase letter
-    '(?=.*[A-Z])'
-    ## check for special characters. Something which is not word, digit or
-    ## space will be treated as special character
-    '(?=.*[^\w\d\s])).'
-    ## Minimum number of characters
-    '{' + str(MINIMUM_PASSWORD_LENGTH) + ',}$')
-
-
 class UserResource(ModelResource):
     raw_password = fields.CharField(attribute=None, readonly=True, blank=True, null=True)
 
     class Meta:
-        # authentication = MultiAuthentication(BasicAuthentication(), ApiKeyAuthentication())
-        authentication = ApiKeyAuthentication()
+        authentication = MultiAuthentication(BasicAuthentication(), ApiKeyAuthentication())
+        # authentication = ApiKeyAuthentication()
         authorization = Authorization()
         allowed_methods = ['get', 'patch', 'put', ]
         always_return_data = True
@@ -128,18 +128,15 @@ class UserResource(ModelResource):
         excludes = ['is_active', 'is_staff', 'is_superuser', 'date_joined', 'last_login']
         resource_name = 'user'
 
-    # def override_urls(self):
-    #     return [
-    #         # url(r"^(?P<resource_name>%s)/login%s$" %
-    #         #     (self._meta.resource_name, trailing_slash()),
-    #         #     self.wrap_view('login'), name="api_login"),
-    #         url(r"^(?P<resource_name>%s)/login%s$" %
-    #             (self._meta.resource_name, trailing_slash()),
-    #             self.wrap_view('login'), name="api_login"),
-    #         url(r'^(?P<resource_name>%s)/logout%s$' %
-    #             (self._meta.resource_name, trailing_slash()),
-    #             self.wrap_view('logout'), name='api_logout'),
-    #     ]
+    def override_urls(self):
+        return [
+            url(r"^(?P<resource_name>%s)/login%s$" %
+                (self._meta.resource_name, trailing_slash()),
+                self.wrap_view('login'), name="api_login"),
+            url(r'^(?P<resource_name>%s)/logout%s$' %
+                (self._meta.resource_name, trailing_slash()),
+                self.wrap_view('logout'), name='api_logout'),
+        ]
 
     # def authorized_read_list(self, object_list, bundle):
     #     return object_list.filter(id=bundle.request.user.id).select_related()
@@ -149,7 +146,6 @@ class UserResource(ModelResource):
     def hydrate(self, bundle):
         print 'hydrate UserResource'
         if "raw_password" in bundle.data:
-            print 'raw_password in bundle.data > '
             # Pop out raw_password and validate it
             # This will prevent re-validation because hydrate is called
             # multiple times
@@ -160,23 +156,6 @@ class UserResource(ModelResource):
             # print bundle.data['user']['raw_password']
             rp = bundle.data["raw_password"]
             del bundle.data['raw_password']
-
-            # Validate password
-            # if not self.validate_password(rp):
-            #     # raise CustomBadRequest(code="invalid password", message="Password must be at least 8 characters with one digit, one uppercase letter and one special character")
-            #     raise CustomBadRequest(code="invalid_password", message="invalid password")
-            #     if len(rp) < MINIMUM_PASSWORD_LENGTH:
-            #         raise CustomBadRequest(
-            #             code="invalid_password",
-            #             message=(
-            #                 "Your password should contain at least {length} "
-            #                 "characters.".format(length=
-            #                                      MINIMUM_PASSWORD_LENGTH)))
-            #     raise CustomBadRequest(
-            #         code="invalid_password",
-            #         message=("Your password should contain at least one number"
-            #                  ", one uppercase letter, one special character,"
-            #                  " and no spaces."))
             bundle.data["password"] = make_password(rp)
             print bundle.data
         return bundle
@@ -185,7 +164,7 @@ class UserResource(ModelResource):
         bundle.data['key'] = bundle.obj.api_key.key
         try:
             del bundle.data['raw_password']
-            #remove bundle.data['password']
+            del bundle.data['password']
         except KeyError:
             print 'KeyError'
         return bundle
@@ -197,31 +176,33 @@ class UserResource(ModelResource):
             print ' >> No profile for this user'
         return super(UserResource, self).get_detail(request, **kwargs)
 
-    # def login(self, request, **kwargs):
-    #     self.method_check(request, allowed=['post'])
+    def login(self, request, **kwargs):
+        self.method_check(request, allowed=['post'])
 
-    #     data = self.deserialize(request, request.raw_post_data, format=request.META.get('CONTENT_TYPE', 'application/json'))
-
-    #     username = data.get('username', '')
-    #     password = data.get('password', '')
-
-    #     user = authenticate(username=username, password=password)
-    #     if user:
-    #         if user.is_active:
-    #             login(request, user)
-    #             return self.create_response(request, {
-    #                 'success': True
-    #             })
-    #         else:
-    #             return self.create_response(request, {
-    #                 'success': False,
-    #                 'reason': 'disabled',
-    #                 }, HttpForbidden )
-    #     else:
-    #         return self.create_response(request, {
-    #             'success': False,
-    #             'reason': 'incorrect',
-    #             }, HttpUnauthorized )
+        data = self.deserialize(request, request.body, format=request.META.get('CONTENT_TYPE', 'application/json'))
+        username = data.get('username', '')
+        password = data.get('password', '')
+        user = authenticate(username=username, password=password)
+        if user:
+            print user.first_name
+            print user.last_name
+            if user.is_active:
+                login(request, user)
+                return self.create_response(request, {
+                    'success': True,
+                    'first_name': user.first_name,
+                    'last_name' : user.last_name
+                })
+            else:
+                return self.create_response(request, {
+                    'success': False,
+                    'reason': 'disabled',
+                    }, HttpForbidden )
+        else:
+            return self.create_response(request, {
+                'success': False,
+                'reason': 'incorrect',
+                }, HttpUnauthorized )
 
 ###############################################################################
 ###############################################################################
@@ -230,8 +211,8 @@ class UserResource(ModelResource):
 class UserProfileResource(ModelResource):
     class Meta:
         queryset = UserProfile.objects.all()
-        # authentication = MultiAuthentication(BasicAuthentication(), ApiKeyAuthentication())
-        authentication = ApiKeyAuthentication()
+        authentication = MultiAuthentication(BasicAuthentication(), ApiKeyAuthentication())
+        # authentication = ApiKeyAuthentication()
         authorization = Authorization()
         always_return_data = True
         allowed_methods = ['get', 'patch', ]
